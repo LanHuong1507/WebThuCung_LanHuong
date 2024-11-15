d09ae5675cf8ddf5a7988cd1fcf4701b8ed4914e
LanHuong <taejoon1507@gmail.com>
2024-11-15 16:58:51 +0700

    update file

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,24 @@ const withTM = require("next-transpile-modules")([
   "rc-picker",
   "rc-input",
   "@ant-design/icons",
-  "@ant-design/icons-svg",
+  "@ant-design/icons-svg", // Transpile the icons SVG module as well
 ]);
 
 module.exports = withTM({
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
-    esmExternals: false,
+    esmExternals: false, // Ensure ESM modules are handled properly
   },
   images: {
-    domains: ['via.placeholder.com'],
+    domains: ['via.placeholder.com'], // Allow external image domains
+  },
+  webpack(config: { externals: any[]; }, { isServer }: any) {
+    if (isServer) {
+      // Fix for SSR issue with certain modules
+      config.externals = ['@ant-design/icons-svg', ...config.externals];
+    }
+
+    return config;
   },
 });
